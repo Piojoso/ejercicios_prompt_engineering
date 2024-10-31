07d5d50a3505850354fe48dd0b43d7b12d00a056
Leonel Bongiovanni <work.leonel.bongiovanni@outlook.com.ar>
2024-10-31 14:52:28 -0300

    Se agrega buscador de recetas

--- gestor-recetas-personal/src/pages/Home.jsx
+++ gestor-recetas-personal/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-// Home.jsx
+import { useState } from 'react'; // Importamos useState
 import { handleLogout } from '../utils/auth.js';
 import { Loader } from '../components/Loader/Loader.jsx';
 import { useProductsCache } from '../context/ProductsCacheContext';
 
 const Home = () => {
   const { products, loading } = useProductsCache();
+  const [searchTerm, setSearchTerm] = useState(''); // Estado para el término de búsqueda
 
   const logout = async () => {
     const result = await handleLogout();
@@ -15,6 +16,11 @@ const Home = () => {
     }
   };
 
+  // Filtramos los productos según el término de búsqueda
+  const filteredProducts = products.filter(product =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="container is-flex is-flex-direction-column is-justify-content-space-between" style={{ minHeight: '100vh' }}>
       <header className="my-5 has-text-centered">
@@ -23,13 +29,31 @@ const Home = () => {
 
       <p className="subtitle has-text-centered mb-5">Your personal recipe book. Add, search, and manage all your favorite recipes!</p>
 
+      {/* Input de Búsqueda */}
+      <div className="field has-addons mb-5">
+        <div className="control is-expanded">
+          <input
+            type="text"
+            className="input"
+            placeholder="Buscar recetas por nombre..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)} // Actualizamos el estado al escribir
+          />
+        </div>
+        <div className="control">
+          <button className="button is-info">
+            Buscar
+          </button>
+        </div>
+      </div>
+
       {loading ? (
         <div className="is-flex is-justify-content-center is-align-items-center" style={{ height: '50vh' }}>
           <Loader />
         </div>
       ) : (
         <div className="columns is-multiline is-centered">
-          {products.map((product) => (
+          {filteredProducts.map((product) => (
             <div key={product.id} className="column is-one-quarter">
               <div className="box has-text-centered">
                 <img src={product.image || 'https://via.placeholder.com/150'} alt={product.title} className="image mb-3" />
